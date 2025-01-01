8a4a8b4dc87935d6123adc103dfab1425b53aca0
HellReys <wallpapertopia@gmail.com>
2025-01-01 18:38:49 +0200

    lots of things

--- network_scan.py
+++ network_scan.py
@@ -1,6 +1,6 @@
+import os
 from flask import Flask, request, jsonify
 import logging
-import os  # Ortam değişkeni için os modülünü ekle
 
 # Basic setup for logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
@@ -14,21 +14,22 @@ def scan_network():
     # Simulated scan result
     return {"devices": ["192.168.1.1", "192.168.1.2"]}
 
+
+
+
 # Route for triggering a network scan
 @app.route('/api/scan', methods=['POST'])
 def api_scan():
     api_key = request.headers.get('Authorization')
-    expected_api_key = os.getenv('API_KEY')
-
-    # API anahtarlarını logla
-    logging.info(f"Received API Key: {api_key}")
-    logging.info(f"Expected API Key: {expected_api_key}")
+    expected_api_key = os.getenv('NETWORK_SCAN_API_KEY')
 
     if not api_key or api_key != expected_api_key:
         return jsonify({"error": "Unauthorized"}), 401
 
     try:
         logging.info("API scan requested.")
+
+        # Perform network scan
         result = scan_network()
         return jsonify(result)
     except Exception as e:
@@ -37,5 +38,5 @@ def api_scan():
 
 
 if __name__ == "__main__":
-    # Buradaki kodu sadece Gunicorn çalıştıracak
     pass
+    #app.run(debug=True, host='0.0.0.0', port=5000)
